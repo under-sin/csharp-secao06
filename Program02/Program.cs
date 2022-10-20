19f1858fb1667416d4303bcd58c1757221f5c5a2
Anderson Nascimento <anderson.nascimento@delage.com.br>
2022-10-19 22:57:57 -0300

    first commit

---
+++ Program02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Program02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double? x = null;
+            double? y = 10.0;
+
+            double a = x ?? 5;
+            double b = y ?? 5;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
